41307cb599faffd8dbf51bdd275f367b2a2354e2
heuzin <matheusqs.arq@gmail.com>
2021-02-08 21:50:42 -0500

    makeGuess prototype for hangman gae

--- hangman-app/hangman-app.js
+++ hangman-app/hangman-app.js
@@ -18,8 +18,26 @@ Hangman.prototype.getPuzzle = function() {
     return puzzle
 }
 
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+
+    if (isUnique) {
+        this.guessedLetters.push(guess)
+    }
+
+    if (isUnique && isBadGuess) {
+        this.remainingGuesses--
+    }
+}
+
 const game1 = new Hangman('cat', 2)
+game1.makeGuess('c')
+game1.makeGuess('t')
+game1.makeGuess('z')
 console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
 
 const game2 = new Hangman('New Jersey', 4)
 console.log(game2.getPuzzle())
