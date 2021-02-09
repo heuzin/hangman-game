26980c00ee8624a9552c0fb7e5958b9dc9378145
heuzin <matheusqs.arq@gmail.com>
2021-02-08 23:22:16 -0500

    showing status message on screen

--- hangman-app/app.js
+++ hangman-app/app.js
@@ -3,11 +3,11 @@ const guessEl = document.querySelector('#guesses')
 const game1 = new Hangman('cat', 2)
 
 puzzleEl.textContent = game1.getPuzzle()
-guessEl.textContent = game1.remainingGuesses
+guessEl.textContent = game1.getStatusMessage()
 
 window.addEventListener('keypress' , e => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.getPuzzle()
-    guessEl.textContent = game1.remainingGuesses
+    guessEl.textContent = game1.getStatusMessage()
 })
--- hangman-app/hangman-app.js
+++ hangman-app/hangman-app.js
@@ -23,6 +23,16 @@ Hangman.prototype.calculateStatus = function() {
     }
 }
 
+Hangman.prototype.getStatusMessage = function() {
+    if (this.status === 'playing') {
+        return `Guesses left: ${this.remainingGuesses}`
+    } else if (this.status === 'failed') {
+        return `Nice try! The word was "${this.word.join('')}"`
+    } else {
+        return 'Great work! You guessed the word.'
+    }
+}
+
 Hangman.prototype.getPuzzle = function() {
     let puzzle = ''
 
@@ -41,6 +51,10 @@ Hangman.prototype.makeGuess = function (guess) {
     guess = guess.toLowerCase()
     const isUnique = !this.guessedLetters.includes(guess)
     const isBadGuess = !this.word.includes(guess)
+
+    if (this.status !== 'playing') {
+        return
+    }
 
     if (isUnique) {
         this.guessedLetters.push(guess)
