b17457ea07d6a5ef1a83cb904012444840085951
heuzin <matheusqs.arq@gmail.com>
2021-02-09 22:52:49 -0500

    converting callback request to promise

--- hangman-app/requests.js
+++ hangman-app/requests.js
@@ -1,16 +1,16 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount) => new Promise((res, rej) => {
     // Making an HTTP request
     const request = new XMLHttpRequest()
 
     request.addEventListener('readystatechange', e => {
         if (e.target.readyState === 4 && e.target.status === 200) {
             const data = JSON.parse(e.target.responseText)
-            callback(undefined, data.puzzle)
+            res(data.puzzle)
         } else if (e.target.readyState === 4) {
-            callback('An error has taken place', undefined)
+            rej('An error has taken place')
         }
     })
 
-    request.open('GET', 'http://puzzle.mead.io/puzzle')
+    request.open('GET', `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     request.send()
-}
+})
